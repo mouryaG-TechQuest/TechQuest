aa274ab85fe917691f70f069ffb9ade378342599
mouryaG-TechQUest <mouryagottipati2@gmail.com>
2025-11-14 09:07:29 -0500

    Hey i am comminting week4 Day5 tasks

---
+++ Week4/Day5/CheckingAccount.java
@@ -0,0 +1,34 @@
+package Day5;
+public class CheckingAccount extends Account{
+    private double overdraftLimit;
+
+    public CheckingAccount(String accountNumber, double balance, String accountHolderName, double overdraftLimit) {
+        super(accountNumber, balance, accountHolderName, "Checking");
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    public double getOverdraftLimit() {
+        return overdraftLimit;
+    }
+
+    public void setOverdraftLimit(double overdraftLimit) {
+        this.overdraftLimit = overdraftLimit;
+    }
+    // Override methods 
+    @Override 
+    public void withdraw(double amount) throws InsufficientFundsException{
+        if(amount > 0 && amount <= (getBalance() + overdraftLimit)){
+            setBalance(getBalance() - amount);
+            System.out.println("Withdrew: " + amount);
+        } else {
+            throw new InsufficientFundsException("Exceeded overdraft limit or invalid amount.");
+        }
+    }
+
+    @Override
+    public void displayDetails(){
+        super.displayDetails();
+        System.out.println("Overdraft Limit: " + overdraftLimit);
+    }
+    
+}
